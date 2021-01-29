80a6d67837a98841b482d0c1ed24172440d4ca27
ophirus <dreckguy@gmail.com>
2021-01-29 21:20:39 +0200

    fix clean script

--- clean.sh
+++ clean.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-aws cloudformation delete-stack --stack-name 'montara-assignment'
+PROJECT='montara-assignment'
+aws s3 rm s3://$PROJECT --recursive
+aws cloudformation delete-stack --stack-name $PROJECT
